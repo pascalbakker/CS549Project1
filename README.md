bcec9e09c60c2537648198980a0031b660dee560
pascalbakker <pascalsbakker@gmail.com>
2020-09-18 16:33:23 -0400

    Commiting skeleton project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project 1 Big Data
+Homework 1 for big data. Hadoop Query stuff.
+
+### Build
+
+	mvn clean compile
+
+### Run
+
+	mvn exec:java -Dexec.mainClass=edu.wpi.project1.App
---
+++ src/main/java/edu/wpi/project1/App.java
@@ -0,0 +1,53 @@
+package edu.wpi.project1;
+
+/**
+ * Project 1
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    // TODO Creating Transaction
+    public static void createTransaction(){}
+
+    // TODO Create Customer
+    public static void createCustomer(){}
+
+
+    /**
+        MAP REDUCE JOBS
+    */
+
+    // TODO Query 2
+    public static void query2(){}
+
+    // TODO Query 3
+    public static void query3(){}
+
+    // TODO Query 4
+    public static void query4(){}
+
+    // TODO Query 5
+    public static void query5(){}
+
+    /**
+        APACHE PIG JOBS
+    */
+
+    // TODO Query 2
+    public static void query2(){}
+
+    // TODO Query 3
+    public static void query3(){}
+
+    // TODO Query 4
+    public static void query4(){}
+
+    // TODO Query 5
+    public static void query5(){}
+
+
+}
