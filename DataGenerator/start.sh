d93a1cf17169f1d088fbd45468cd8569dcbe4dc2
pascalbakker <pascalsbakker@gmail.com>
2020-09-19 11:59:38 -0400

    Fixed path to relative in DataGenerator, added gitignore

--- DataGenerator/start.sh
+++ DataGenerator/start.sh
@@ -2,5 +2,5 @@ javac DataGenerator.java
 java DataGenerator
 
 hadoop dfs -mkdir /user/hadoop/Project1/data
-hadoop dfs -copyFromLocal Transactions.txt /user/hadoop/Project1/data
-hadoop dfs -copyFromLocal Customers.txt /user/hadoop/Project1/data
+hadoop dfs -copyFromLocal Transactions.txt ../data
+hadoop dfs -copyFromLocal Customers.txt ../data
