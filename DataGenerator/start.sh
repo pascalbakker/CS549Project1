142e05ebfa683dad468e194d0c0efb109719c493
pascalbakker <pascalsbakker@gmail.com>
2020-09-19 12:01:14 -0400

    Fixed absolute path

--- DataGenerator/start.sh
+++ DataGenerator/start.sh
@@ -1,6 +1,6 @@
 javac DataGenerator.java
 java DataGenerator
 
-hadoop dfs -mkdir /user/hadoop/Project1/data
+hadoop dfs -mkdir ../data
 hadoop dfs -copyFromLocal Transactions.txt ../data
 hadoop dfs -copyFromLocal Customers.txt ../data
